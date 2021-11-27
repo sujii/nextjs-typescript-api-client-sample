0c1036deac347feff34c5f98559256738cc50345
Susumu Fujii <powermac.g7@gmail.com>
2021-11-28 05:33:47 +0900

    fix: eslint format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
+import Head from "next/head";
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { usePostalCodeApi } from '../src/hooks/postalCodeApi';
-import styles from './styles/Home.module.css';
+import { usePostalCodeApi } from "../src/hooks/postalCodeApi";
+import styles from "./styles/Home.module.css";
 import { GetAddressFromZip } from "../src/modules/postalCode";
 
 const IndexPage: React.FC<IndexPageProps> = () => {
@@ -10,26 +10,26 @@ const IndexPage: React.FC<IndexPageProps> = () => {
   const { address } = useSelector((state: IReduxState) => state.address);
   const [zipCode, setZipCode] = useState<number | string>(undefined);
 
-  const onChangeField = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const text = e.target.value;
-    console.log(text);
-    setZipCode(text);
-  }, []);
+  const onChangeField = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const text = e.target.value;
+      console.log(text);
+      setZipCode(text);
+    },
+    []
+  );
 
   const { addressData } = usePostalCodeApi(zipCode);
   const addressResults = JSON.stringify(addressData);
 
-  console.dir('addressData ////////');
+  console.dir("addressData ////////");
   console.dir(addressData);
 
   const getAddress = useCallback(() => {
     if (zipCode) {
       dispatch(GetAddressFromZip(zipCode));
     }
-  }, [
-    dispatch,
-    zipCode
-  ]);
+  }, [dispatch, zipCode]);
 
   return (
     <div className={styles.container}>
@@ -51,13 +51,10 @@ const IndexPage: React.FC<IndexPageProps> = () => {
         </p>
 
         <p className={styles.requestButton}>
-          <button
-            type="button"
-            name="requestFromCodes"
-            onClick={getAddress}
-          >Get an address by your code</button>
+          <button type="button" name="requestFromCodes" onClick={getAddress}>
+            Get an address by your code
+          </button>
         </p>
-
       </main>
     </div>
   );
