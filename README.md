4847ade5111d6f3bae15764717075a7ed3642032
Susumu Fujii <powermac.g7@gmail.com>
2021-11-30 18:20:37 +0900

    modify: css to scss

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # nextjs-typescript-api-client-sample
-
 api-client-UI entry-level testing sample
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { setAppElement } from "react-modal";
 import configureStore from "../src/config/store";
-import "./styles/globals.css";
+import "./styles/globals.scss";
 
 const store = configureStore();
 
